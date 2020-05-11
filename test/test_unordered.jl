28b2ff3a78f8edac2808a7de9c470cec4cef9b36
Takafumi Arakaki <aka.tkf@gmail.com>
2020-05-11 14:55:34 -0700

    Support Set, keys, and values (#26)

--- test/test_unordered.jl
+++ test/test_unordered.jl
@@ -2,7 +2,7 @@ module TestUnordered
 
 import SplittablesBase
 
-raw_examples = """
+raw_examples = dict_examples = """
 Dict{Symbol,Int}()
 Dict(:a => 1)
 Dict(:a => 1, :b => 2)
@@ -10,6 +10,25 @@ Dict(:a => 1, :b => 2, :c => 3)
 Dict(:a => 1, :b => 2, :c => 3, :d => 4)
 Dict(:a => 1, :b => 2, :c => 3, :d => 4, :e => 5)
 Dict(zip('a':'z', 1:26))
+"""
+
+function wrapdict(f)
+    lines = map(split(dict_examples, "\n", keepempty = false)) do code
+        "$f($code)"
+    end
+    return join(lines, "\n") * "\n"
+end
+
+raw_examples *= wrapdict("keys") * wrapdict("values")
+
+raw_examples *= """
+Set(1:0)
+Set(1:1)
+Set(1:2)
+Set(1:3)
+Set(1:4)
+Set(1:5)
+Set(1:100)
 """
 
 # An array of `(label = ..., data = ...)`
